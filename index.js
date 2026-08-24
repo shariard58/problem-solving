7089700842895fda49e1d23e7abfdfcc667be596
shariard58 <shariard58@gmail.com>
2026-08-24 23:09:26 +0600

    two sum problem

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("the is shariar");
+const nums = [3, 3];
+const target = 6;
+const results = [];
+
+let obj = {};
+
+for (let x = 0; x < nums.length; x++) {
+  let need = target - nums[x];
+  if (obj.hasOwnProperty(need)) {
+    results.push(obj[need]);
+    results.push(x);
+  } else {
+    obj[nums[x]] = x;
+  }
+}
+
+console.log("The results is", results);

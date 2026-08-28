64d6f574167e0a268bf049e06c0499af5cc0e966
shariard58 <shariard58@gmail.com>
2026-08-28 15:52:40 +0600

    feat : move zeros to the button problmem

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("the is shariar");
+const nums = [0, 0, 1];
+
+let left = 0;
+let right = left + 1;
+
+while (right < nums.length) {
+  if (nums[left] == 0 && nums[right] != 0) {
+    [nums[left], nums[right]] = [nums[right], nums[left]];
+    left++;
+  }
+
+  right++;
+}
+
+console.log("The nums value is", nums);

085a0aee78285472e462b392ee92106b50522f9a
shariard58 <shariard58@gmail.com>
2026-08-28 22:22:55 +0600

    fix : find first unique chracer

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("the is shariar");
+let s = "leetcode";
+let obj = {};
+
+// expected output 0, the index of the l
+
+function findUniqueCharacter() {
+  for (let x = 0; x < s.length; x++) {
+    if (obj.hasOwnProperty(s[x])) {
+      obj[s[x]] = obj[s[x]] + 1;
+    } else {
+      obj[s[x]] = 1;
+    }
+  }
+
+  console.log("the object is", obj);
+
+  for (let x = 0; x < s.length; x++) {
+    if (obj[s[x]] == 1) {
+      return x;
+    }
+  }
+
+  return -1;
+}
+
+console.log(findUniqueCharacter());

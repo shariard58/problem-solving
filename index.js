7e349677923bf1af2f592b484f0e6e3c0fb3f3ad
shariard58 <shariard58@gmail.com>
2026-08-28 15:02:08 +0600

    feat : best time to buy and sell stock

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("this is shariar");
+const prices = [3, 2, 6, 1, 4, 7];
+let buy = 0;
+let sell = buy + 1;
+let profit = 0;
+let maxProfit = 0;
+
+while (sell < prices.length) {
+  if (prices[sell] < prices[buy]) {
+    buy = sell;
+  } else {
+    profit = prices[sell] - prices[buy];
+    maxProfit = Math.max(maxProfit, profit);
+  }
+  console.log("Buy value is", prices[buy]);
+  console.log("Sell value is", prices[sell]);
+  sell++;
+}
+
+console.log("Max profit is", maxProfit);

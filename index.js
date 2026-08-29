040c37d27824fc44405deb73759d3b4d9e3bb19d
shariard58 <shariard58@gmail.com>
2026-08-29 06:21:09 +0600

    fix : find the pivot index

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("the is shariar");
+const nums = [1, 7, 3, 6, 5, 6];
+// Output: 3
+
+function findPivot(nums) {
+  const arraySum = nums.reduce((acc, cuu) => acc + cuu, 0);
+  let leftSum = 0;
+  let rightSum = 0;
+  let pivotIndex = -1;
+
+  for (let x = 0; x < nums.length; x++) {
+    rightSum = arraySum - (leftSum + nums[x]);
+    if (leftSum == rightSum) {
+      pivotIndex = x;
+      break;
+    }
+
+    leftSum = leftSum + nums[x];
+  }
+
+  return pivotIndex;
+}
+
+console.log(findPivot(nums));

293faed6bd48fd45502501c2714fcbc31ba70518
shariard58 <shariard58@gmail.com>
2026-09-01 20:22:33 +0600

    fix : intersection of two arrays

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("the is shariar");
+
+const nums1 = [1, 2, 2, 1];
+const nums2 = [2, 2];
+
+function interSectionOfArrays(nums1, nums2) {
+  const obj = {};
+  const results = [];
+  for (let x = 0; x < nums1.length; x++) {
+    if (!obj.hasOwnProperty(nums1[x])) {
+      obj[nums1[x]] = 1;
+    }
+  }
+
+  for (let x = 0; x < nums2.length; x++) {
+    if (obj.hasOwnProperty(nums2[x])) {
+      results.push(nums2[x]);
+    }
+  }
+
+  return [...new Set(results)];
+}
+
+console.log(interSectionOfArrays(nums1, nums2));
